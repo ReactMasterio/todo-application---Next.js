f034de58d8a5a363218119e9b1066c7460ce7c9f
Habib <habibbehdarvandi.dev@gmail.com>
2023-11-22 03:24:06 -0800

    Make Website Responsive

--- app/Components/Table.tsx
+++ app/Components/Table.tsx
@@ -1,27 +1,62 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Image from 'next/image'
-import delete_icon from '@/public/images/delete.svg'
-import check_icon from '@/public/images/check.svg'
+import Image from 'next/image';
+import delete_icon from '@/public/images/delete.svg';
+import check_icon from '@/public/images/check.svg';
 
-const Table = ({ data }: any) => {
+interface FormData {
+    id: number;
+    title: string;
+    dueDate: string;
+    checked: boolean;
+}
+
+interface TableProps {
+    data: FormData[];
+    setTableData: React.Dispatch<React.SetStateAction<FormData[]>>;
+}
+
+const Table: React.FC<TableProps> = ({ data, setTableData }) => {
+
+    const handleDelete = (index: number) => {
+        const newData = [...data];
+        newData.splice(index, 1);
+        setTableData(newData);
+    };
+
+    const handleCheck = (index: number) => {
+        const newData = [...data];
+        newData[index].checked = !newData[index].checked;
+        setTableData(newData);
+
+    };
+
+
     return (
         <table className="bg-white shadow-sm rounded-md w-[850px] overflow-y-scroll overflow-x-hidden self-center">
             <thead className='border-b-2 border-b-gray-100'>
-                <th className='py-4'>ToDo Title</th>
-                <th>Due Date</th>
-                <th>Actions</th>
+                <tr>
+                    <th className='py-4'>ToDo Title</th>
+                    <th>Due Date</th>
+                    <th>Actions</th>
+                </tr>
             </thead>
             <tbody className='text-center'>
-                {data.map((item: any, index: any) => (
-                    <tr key={index}>
+                {data.map((item: FormData, index: number) => (
+                    <tr key={index} style={{ textDecoration: item.checked ? 'line-through' : 'none' }}>
                         <td className='py-4'>{item.title}</td>
                         <td>{item.dueDate}</td>
                         <td>
-                            <button className='bg-red-500 hover:bg-red-600 p-3 rounded-lg mx-4 transition-all duration-300'>
+                            <button
+                                className='bg-red-500 hover:bg-red-600 p-3 rounded-lg mx-4 transition-all duration-300'
+                                onClick={() => handleDelete(index)}
+                            >
                                 <Image alt='DELETE Icon' src={delete_icon} />
                             </button>
-                            <button className='bg-green-800 hover:bg-green-700 transition-all duration-300 p-3 rounded-lg mx-4'>
+                            <button
+                                className='bg-green-800 hover:bg-green-700 transition-all duration-300 p-3 rounded-lg mx-4'
+                                onClick={() => handleCheck(index)}
+                            >
                                 <Image alt='check Icon' src={check_icon} />
                             </button>
                         </td>
@@ -30,15 +65,6 @@ const Table = ({ data }: any) => {
             </tbody>
         </table>
     );
-};
-
-Table.propTypes = {
-    data: PropTypes.arrayOf(
-        PropTypes.shape({
-            title: PropTypes.string,
-            dueDate: PropTypes.string,
-        })
-    ).isRequired,
 };
 
 export default Table;
--- app/page.tsx
+++ app/page.tsx
@@ -5,34 +5,29 @@ import add_icon from '@/public/images/add.svg'
 import Table from './Components/Table'
 import { useState } from 'react'
 
-
-
-
-
+interface FormData {
+  title: any;
+  dueDate: any;
+}
 
 export default function Home() {
-  const [tableData, setTableData]: any = useState([]);
-
+  const [tableData, setTableData]: any = useState<FormData[]>([]);
   const data: any = [];
 
-
-
-  const handleSubmit = (e: any) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const title: any = document.getElementById("todo");
-    const dueDate: any = document.getElementById("date");
+    const title = document.getElementById("todo") as HTMLInputElement;
+    const dueDate = document.getElementById("date") as HTMLInputElement;
 
-    const getFormData = (title: HTMLInputElement, dueDate: HTMLInputElement) => {
-      const formData = {
+    const getFormData = (title: any, dueDate: any) => {
+      const formData: FormData = {
         title: title.value,
         dueDate: dueDate.value,
       };
 
       if (formData.title && formData.dueDate) {
-        data.push(formData);
-        console.log(data);
-        setTableData([...data]);
+        setTableData((prevData: FormData[]) => [...prevData, formData]);
         console.log(tableData);
       }
 
@@ -42,24 +37,23 @@ export default function Home() {
 
     // Call the function
     getFormData(title, dueDate);
-
-  }
+  };
 
 
   return (
-    <main className="flex flex-col justify-around align-middle">
-      <div className='bg-white px-4 py-4 mx-6 my-4 rounded-2xl shadow-md w-[650px] self-center flex'>
-        <button>
+    <main className="flex flex-col justify-around items-center">
+      <div className='bg-white px-4 py-4 mx-6 my-4 rounded-2xl shadow-md w-full md:w-[650px] flex flex-row '>
+        <button className='mb-2 md:mb-0 w-fit'>
           <Image alt='search icon' src={search_icon} />
         </button>
-        <input type='text' placeholder='search...' className='w-full mx-4 text-gray-800' />
+        <input type='text' placeholder='search...' className='w-full md:mx-4 text-gray-800' />
       </div>
 
       <div className='divider'></div>
 
       <div className="container">
-        <form className='flex' onSubmit={handleSubmit} action="">
-          <div className="form-itam flex flex-col w-1/6 mr-12">
+        <form className='flex flex-col md:flex-row' onSubmit={handleSubmit} action="">
+          <div className="form-itam flex flex-col mb-4 md:mb-0 md:mr-12">
             <label htmlFor="todo" className='font-semibold mb-2 ml-2'>Todo Title :</label>
             <input type="text" name="todo" id="todo"
               className='bg-white rounded-lg border-slate-400 shadow-md py-3 px-4'
@@ -67,27 +61,25 @@ export default function Home() {
             />
           </div>
 
-          <div className="form-itam flex flex-col w-1/6 mr-12">
+          <div className="form-itam flex flex-col md:w-1/6">
             <label htmlFor="date" className='font-semibold mb-2 ml-2'>Due Date :</label>
             <input type="date" name="date" id="date"
               className='bg-white rounded-lg border-slate-400 shadow-md py-3 px-4 text-gray-400'
             />
           </div>
 
-          <button type="submit" className='bg-green-800 rounded-full flex w-12 h-12 justify-center align-middle self-end'>
+          <button type="submit" className='bg-green-800 rounded-full flex w-full md:w-12 h-12 self-end items-center justify-center'>
             <Image alt='add icon' src={add_icon} className='self-center' />
           </button>
-
-
         </form>
 
         <div className="divider"></div>
 
-
-        <div className="container flex justify-center align-middle">
-          {/* <Table data={tableData} /> */}
+        <div className="container flex justify-center items-center">
+          <Table data={tableData} setTableData={setTableData} />
         </div>
       </div>
     </main>
+
   )
 }
