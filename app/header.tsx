fe301117ec3047101cdddc9d56e8923daf690862
Habib <habibbehdarvandi.dev@gmail.com>
2023-11-21 16:10:42 -0800

    initial & create Header and Search Container

---
+++ app/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const header = () => {
+    return (
+        <header className='bg-white px-8 py-6 mx-8 my-4 rounded-2xl shadow-md'>
+            <h1 className='text-green-800 font-semibold text-lg'>ToDo Application</h1>
+        </header>
+    )
+}
+
+export default header
